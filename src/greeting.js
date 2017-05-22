18e222d4bb99c70bb5e819d8860441adfbad1f05
richardcurtis <rcurtis@adobe.com>
2017-05-22 14:55:37 +0100

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -7,5 +7,12 @@
 function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'i/o insights Richard';
+    
+    var request = require("request");
+request("http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/'", function(error, response, body) {
+});
+    
+
+    
     return {payload:  'Hello, ' + name + ' from ' + place + ' !'};
 }
