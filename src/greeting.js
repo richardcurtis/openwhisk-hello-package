27faa4a5fa64a446eb1a20f348e8ee80a505893a
richardcurtis <rcurtis@adobe.com>
2017-05-22 21:38:39 +0100

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -4,12 +4,17 @@
  * @param name A person's name.
  * @param place Where the person is from.
  */
+var http = require('http');
+
 function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'i/o insights Richard v0.1';
     //challenge 
-    var method = request.method;
-    const request = require('request');
+    
+    var method = "";
+    var server = module.exports = http.createServer(function (req, res) {
+        method = req.headers['method']
+    }
 
 const options = {  
     url: 'http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/',
