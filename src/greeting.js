a3b01e2d2ea3441f9d103fe0e6979f706aae85e0
richardcurtis <rcurtis@adobe.com>
2017-05-22 21:58:32 +0100

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -13,22 +13,18 @@ qs = require('querystring');
 function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'i/o insights Richard v0.1';
+    
     //challenge 
     
     http.createServer(function (req, res) {
     if(req.method=='POST') {
-            var body='';
-            req.on('data', function (data) {
-                body +=data;
+           
             });
-            req.on('end',function(){
-                var POST =  qs.parse(body);
-                console.log(POST);
+           
             });
     }
     else if(req.method=='GET') {
-        var url_parts = url.parse(req.url,true);
-        console.log(url_parts.query);
+        
     }
 });
     
@@ -40,12 +36,6 @@ const options = {
     
 };
 
-
-
-request(options, function(err, res, body) {  
-    let json = JSON.parse(body);
-    console.log(json);
-});
     
    
     
