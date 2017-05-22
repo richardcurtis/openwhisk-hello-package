4cb887b9ebdc1161977c687237a1d01cf5e3a372
richardcurtis <rcurtis@adobe.com>
2017-05-22 19:28:20 +0100

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -8,9 +8,25 @@ function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'i/o insights Richard';
     
-    var request = require("request");
-    request("http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/'", function(error, response, body) {
-    });
+    const request = require('request');
+
+const options = {  
+    url: 'http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/'
+        method: 'POST',
+    json: {
+    "message": "Hello From GitHub"
+    }
+    
+};
+
+request(options, function(err, res, body) {  
+    let json = JSON.parse(body);
+    console.log(json);
+});
+    
+    //var request = require("request");
+    //request("http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/'", function(error, response, body) {
+    //});
     
 
     
