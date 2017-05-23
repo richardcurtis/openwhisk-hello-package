2ef206c3b182f3aef260347b0c42eb4b9d32a497
richardcurtis <rcurtis@adobe.com>
2017-05-23 23:24:24 +0100

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -5,44 +5,47 @@
  * @param place Where the person is from.
  */
 
-var sys = require ('sys'),
-url = require('url'),
-http = require('http'),
-qs = require('querystring');
- var method = '';
+//var sys = require ('sys'),
+//url = require('url'),
+//http = require('http'),
+//qs = require('querystring');
+// var method = '';
 
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
 
 function main(params) {
    
-    var name = params.name || params.payload || 'stranger';
-    
-    
-    //challenge 
-//     app.get('/', function(req, res) {
-//      method = 'User-Agent: ' + req.headers['user-agent']
-//});
-     //var method = params.__ow_method;
-     //var pathname = url.parse(request.url).pathname;
- function onRequest(request, response) {
-var pathname = url.parse(request.url).pathname;         // I don't understand the pathname property
-}
- 
+    var name = params.name || params.payload || 'stranger'; 
     var place = params.place || 'i/o insights Richard v0.1' || pathname;
- 
 
-   
-const options = {  
-    url: 'http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/',
-        method: 'POST',
-    json: {"message": "Hello From GitHub v2"}
-    
-};
+	    
+		var rex='';
+		var method = params.__ow_method;
+		if (method=="get" ) {
+		  rex = params.challenge;
+		}
+		if (method=="post") {
+		  rex = "POST sent";
+		}
+		
+    	var x = new Buffer(JSON.stringify(params)).toString();
+	request.post(
+    'http://logs-01.loggly.com/inputs/e156ff2e-23b6-48eb-8090-6a4c46cdec3a/tag/http/',
+    { json: { "message": "Hello From GitHub v2" + x + ":Method=" + method + ", body " + rex} },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+); 
 
     
-   
-    
-
-    
-    //return {payload:  'Hello, ' + name + ' from ' + place + ' !' + method};
-  return { response: params };
+  return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'text/plain' },
+        body: rex,
+    };
 }
